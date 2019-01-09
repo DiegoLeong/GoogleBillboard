ed78817d11b53f0411111d16994480d5cf50a109
dileong <dileong@s.sfusd.edu>
2019-01-09 13:37:18 -0800

    k

--- GoogleBillboard.pde
+++ GoogleBillboard.pde
@@ -1,20 +1,18 @@
-public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
+public final static String e = "2718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
 public void setup()  
-{            
-    String digits = e.substring(2,12);
-	double dNum = Double.parseDouble(digits);
-	for(int i=0; i<e.length; i++)
+{   
+
+	for(int i = 0; i<e.length(); i++)
 	{
-		
-		break;
+		if(isPrime(Double.parseDouble(e.substring(i,i+10))) == true)
+		{
+			System.out.println(Double.parseDouble(e.substring(i,i+10)));
+			break;
+		}
 	}
-	System.out.println(dNum);
 
 }  
-public void draw()  
-{   
-	//not needed for this assignment
-}  
+
 public boolean isPrime(double dNum)  
 {   
     if(dNum < 2)
--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -14,21 +14,21 @@ import java.io.IOException;
 
 public class GoogleBillboard extends PApplet {
 
-public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
+public final static String e = "2718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
 public void setup()  
-{            
-    String digits = e.substring(2,12);
-	double dNum = Double.parseDouble(digits);
-
-
-
-	System.out.println(dNum);
-
-}  
-public void draw()  
 {   
-	//not needed for this assignment
+
+	for(int i = 0; i<e.length(); i++)
+	{
+		if(isPrime(Double.parseDouble(e.substring(i,i+10))) == true)
+		{
+			System.out.println(Double.parseDouble(e.substring(i,i+10)));
+			break;
+		}
+	}
+
 }  
+
 public boolean isPrime(double dNum)  
 {   
     if(dNum < 2)
